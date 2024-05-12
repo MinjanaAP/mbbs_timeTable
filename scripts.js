0f8c497c1f9bde04fb5c304302abfdb31052300b
pasan <pasanathuluwage28@gmail.com>
2024-05-12 19:44:37 +0530

    initial commit for hiru's timetable

---
+++ scripts.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+    
+    $.getJSON('timetable.json', function(scheduleData) {
+        const scheduleDiv = $('#schedule');
+        
+        function displayFullSchedule() {
+            scheduleDiv.empty();
+
+            $.each(scheduleData, function(day, activities) {
+                
+                const dayHeading = $('<h2>').text(day);
+                scheduleDiv.append(dayHeading);
+
+                $.each(activities, function(index, activity) {
+
+                    const activityElement = $('<b>').text(activity.time);
+                    const activityDescription = $('<p>').text(activity.activity);
+                    scheduleDiv.append('<input type="checkbox" id="checkbox" name="checkbox">');
+                    scheduleDiv.append(activityElement);
+                    scheduleDiv.append(activityDescription);
+                });
+            });
+        }
+        displayFullSchedule();
+
+        //! Add click event to the search button
+        $('#searchButton').click(function () {
+            var searchText = $('#searchInput').val().trim();
+            if (searchText !== '') {
+                searchSchedule(searchText);
+            } else {
+                displayFullSchedule();
+            }
+        });
+
+        //? Function to search for a specific day in the schedule
+        function searchSchedule(searchText) {
+            
+            scheduleDiv.empty();
+
+            $.each(scheduleData, function(day, activities) {
+                if (day.toLowerCase().includes(searchText.toLowerCase())) {
+                    const dayHeading = $('<h2>').text(day);
+                    scheduleDiv.append(dayHeading);
+                    
+                    $.each(activities, function(index, activity) {
+                        const activityElement = $('<b>').text(activity.time);
+                        const activityDescription = $('<p>').text(activity.activity);
+                        scheduleDiv.append('<input type="checkbox" id="checkbox" name="checkbox">');
+                        scheduleDiv.append(activityElement);
+                        scheduleDiv.append(activityDescription);
+                    });
+                }
+            });
+        }
+
+        var currentDate = new Date();
+        var targetDate = new Date(currentDate.getFullYear(), 4, 30);
+        var difference = targetDate - currentDate;
+        var daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
+        var hoursLeft = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutesLeft = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        var secondsLeft = Math.floor((difference % (1000 * 60)) / 1000);
+
+        // Display the time left until May 30th
+        $('#days').text(daysLeft);
+        $('#hours').text(hoursLeft);
+        $('#minutes').text(minutesLeft);
+        $('#seconds').text(secondsLeft);
+        $('#timeLeft').text('Time left until May 30th: ' + daysLeft + ' days, ' + hoursLeft + ' hours, ' + minutesLeft + ' minutes, ' + secondsLeft + ' seconds');
+    })
+    .fail(function(error) {
+        console.log('Error loading schedule:', error);
+    });
+});
